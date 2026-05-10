d1695118847193d8a00b15461f41eca1d3e8b5f8
SaulAmador <anthraz.wow@gmail.com>
2026-05-09 22:16:10 -0600

    Fizzbuzz con pruebas Tlaloc V

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,7 +1,18 @@
 export function fizzbuzz(){
   //tu código va aqui
- 
-  
+  // El código de FizzBuzz se ejecutará desde el número 1 hasta el número 100.
+  for (let i = 1; i <= 100; i++) {
+    // Si el número es divisible por 3 y por 5, se imprime "FizzBuzz".
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+      // Si el número es divisible por 3, se imprime "Fizz".
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+      // Si el número es divisible por 5, se imprime "Buzz".
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
 }
-
-fizzbuzz();
